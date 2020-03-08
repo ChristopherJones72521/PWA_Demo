bfbe471eaef7fa85c8ea20463602ef1fe298cd40
Chris Jones <christopherjones72521@gmail.com>
2020-03-08 16:50:32 -0400

    automated code push

--- pwaInstallPrompt.js
+++ pwaInstallPrompt.js
@@ -3,8 +3,8 @@
 /////////////////////////////////////////////////////////////////////////////////
 
 let deferredPrompt;
-const a2hsBtn = document.querySelector("#a2hs-prompt");
 let userSelected = false;
+const a2hsBtn = document.querySelector("#a2hs-prompt");
 
 // Triggers add to home screen prompt (non-iOS)
 function addToHomeScreen() {
@@ -19,22 +19,17 @@ function addToHomeScreen() {
         console.log('user dismissed A2HS prompt')
         userSelected = true;
       }
-      console.log('setting deferredPrompt to NULL')
       deferredPrompt = null;
-      console.log(deferredPrompt);
     })
 }
 
 const dismiss = document.querySelector(".dismiss");
-console.log(dismiss);
 dismiss.addEventListener("click", e => {
-  console.log('the dismiss button was clicked');
   a2hsBtn.style.display = "none";
 })
 
 // Displays the prompt to install and will trigger installation if clicked
 function showAddToHomeScreen() {
-  console.log("the showAddToHomeScreen function is being called");
   // display is none by default. This will display the prompt
   a2hsBtn.style.display = "block";
   a2hsBtn.addEventListener("click", addToHomeScreen);
@@ -45,7 +40,6 @@ window.addEventListener('beforeinstallprompt', e => {
   // This if statment is only necessary to prevent the prompt from reappearing
   // Would be better to store this selection in a cookie
   if (!userSelected) {
-    console.log('beforeinstallprompt fired');
     // Prevent Chrome 67 and earlier from automatically showing the prompt
     e.preventDefault();
     // Stash the event so it can be triggered later.
